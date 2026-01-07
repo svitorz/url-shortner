dcaee1625176cc8eb59b2fe46452e58a5e68f29a
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-01-07 18:16:11 -0300

    refac: separando logica do rate limiter

--- api/internal/requests/router.go
+++ api/internal/requests/router.go
@@ -3,17 +3,15 @@ package requests
 import (
 	"svitorz/url-shortner/internal/requests/middleware"
 	"svitorz/url-shortner/internal/requests/routes"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
 )
 
-func SetupRouter(redisClient *redis.Client, limit int, window time.Duration) *gin.Engine {
+func SetupRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 
-	r.Use(middleware.RateLimiter(redisClient, limit, window))
+	r.Use(middleware.RateLimiterMiddleware())
 
 	return routes.Config(r)
 }
