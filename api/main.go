6ac9cd0a140bc6359161f7896db432a56a450901
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-01-07 18:16:16 -0300

    refac: separando logica do rate limiter

--- api/main.go
+++ api/main.go
@@ -2,29 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"svitorz/url-shortner/internal/config"
 	"svitorz/url-shortner/internal/requests"
-	"time"
 )
 
 func main() {
 	fmt.Println("Iniciando execucao")
-	cfg, err := config.LoadConfig()
-	fmt.Println("Carregando configuracoes")
+	r := requests.SetupRouter()
+	fmt.Println("setup router")
+	port, err := config.LoadAppConfig()
 	if err != nil {
-		fmt.Println("Erro ao carregar arquivos do .env", err)
+		fmt.Println("Erro ao carregar confg da aplicacao", err)
 	}
 
-	fmt.Println(cfg)
-
-	rdb, err := config.ConnectRedis(cfg)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rdb.Close()
-
-	r := requests.SetupRouter(rdb, 10, time.Minute)
-	r.Run(fmt.Sprintf(":%d", cfg.AppPort))
+	fmt.Println("carregou configuracoes")
+	r.Run(fmt.Sprintf(":%d", port))
 }
