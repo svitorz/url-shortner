9e09b7458b86fac25cb5180d37040236a4b6ea64
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-01-07 19:07:18 -0300

    fix: set foreign keys

--- api/internal/models/links.go
+++ api/internal/models/links.go
@@ -1,8 +1,19 @@
 package models
 
+import "time"
+
 type Link struct {
-	slug       string
-	target_url string
-	owner      User
-	is_active  bool
+	ID        uint   `gorm:"primaryKey"`
+	Slug      string `gorm:"size:64;uniqueIndex"`
+	TargetURL string `gorm:"column:target_url"`
+	IsActive  bool   `gorm:"default:true"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	// FK para User
+	OwnerID uint `gorm:"not null;index"`
+	Owner   User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:OwnerID;references:ID"`
+
+	// Relação 1:N com Click
+	//Clicks []Click `gorm:"foreignKey:LinkID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
--- api/internal/models/user.go
+++ api/internal/models/user.go
@@ -6,11 +6,13 @@ import (
 
 // gorm.Model definition
 type User struct {
-	ID        uint    `json:"id,omitempty" gorm:"primaryKey"`
-	Name      string  `json:"full_name"`
-	Email     *string `json:"email"`
-	Password  string
+	ID        uint      `json:"id,omitempty" gorm:"primaryKey"`
+	Name      string    `json:"full_name"`
+	Email     *string   `json:"email" gorm:"uniqueIndex"`
+	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	Link      []Link
+
+	// Relação 1:N com Link usando OwnerID como FK no lado de Link
+	Links []Link `gorm:"foreignKey:OwnerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
 }
