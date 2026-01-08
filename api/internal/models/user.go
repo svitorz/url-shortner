978b6f49eeb65f358bf02dd91ffdef7203334566
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-01-08 11:17:49 -0300

    fix: impedir emails null

--- api/internal/models/user.go
+++ api/internal/models/user.go
@@ -8,7 +8,7 @@ import (
 type User struct {
 	ID        uint      `json:"id,omitempty" gorm:"primaryKey"`
 	Name      string    `json:"full_name"`
-	Email     *string   `json:"email" gorm:"uniqueIndex"`
+	Email     string    `json:"email" gorm:"uniqueIndex"`
 	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
