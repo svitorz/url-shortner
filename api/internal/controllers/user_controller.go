e6286f2dc9818c3a069f8bfe61e880bfe0aea64d
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-01-08 12:51:37 -0300

    feat: funcao para atualizar usuario

--- api/internal/controllers/user_controller.go
+++ api/internal/controllers/user_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"strings"
 	"svitorz/url-shortner/internal/auth"
 	"svitorz/url-shortner/internal/models"
@@ -13,36 +14,93 @@ import (
 )
 
 func CreateUser(c *gin.Context) {
-	var user models.User
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+	var input struct {
+		Name     string `json:"full_name" binding:"required"`
+		Email    string `json:"email" binding:"required,email"`
+		Password string `json:"password" binding:"required,min=8"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
 		return
 	}
 
-	user.CreatedAt = time.Now()
-	user.UpdatedAt = time.Now()
-
-	encryptedPassword, err := auth.HashPassword(user.Password)
+	encryptedPassword, err := auth.HashPassword(input.Password)
 	if err != nil {
-		c.JSON(500, gin.H{"error": "Error encrypting password"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error encrypting password"})
 		return
 	}
-	user.Password = encryptedPassword
+
+	email := input.Email
+	user := models.User{
+		Name:      input.Name,
+		Email:     &email,
+		Password:  encryptedPassword,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
 
 	result := repository.DB.Create(&user)
 	if result.Error != nil {
 		if strings.Contains(result.Error.Error(), "idx_users_email") {
-			c.JSON(400, gin.H{"error": "Email already in use"})
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Email already in use"})
 			return
 		}
-		c.JSON(500, gin.H{"error": result.Error.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": result.Error.Error()})
 		return
 	}
 	log.Printf("User created: %v", user)
-	c.JSON(201, "User created successfully")
+	c.JSON(http.StatusCreated, "User created successfully")
 }
 
-func UpdateUser(c *gin.Context) {}
+func UpdateUser(c *gin.Context) {
+	var bind struct {
+		ID uint `uri:"id" binding:"required"`
+	}
+	var input struct {
+		Name  string `json:"full_name" binding:"required"`
+		Email string `json:"email" binding:"required,email"`
+	}
+
+	if err := c.ShouldBindUri(&bind); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := auth.ExtractTokenID(c)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error:": err.Error()})
+		return
+	}
+
+	if id != bind.ID {
+		c.JSON(http.StatusForbidden, gin.H{"unauthorized": "you have not permission to do that"})
+		return
+	}
+
+	var user models.User
+
+	repository.DB.First(&user, id)
+
+	user.Name = input.Name
+	user.Email = &input.Email
+	user.UpdatedAt = time.Now()
+
+	result := repository.DB.Save(&user)
+
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+	log.Printf("User updated: %v", user)
+	c.JSON(http.StatusAccepted, "User updated successfully")
+}
 
 func DeleteUser(c *gin.Context) {}
 
@@ -52,13 +110,13 @@ func Login(c *gin.Context) {
 		Password string `json:"password"`
 	}
 	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	var user models.User
 	result := repository.DB.Where("email = ?", input.Email).First(&user)
 	if result.Error != nil {
-		c.JSON(401, gin.H{"error": "Invalid email or password"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email or password"})
 		fmt.Println("Erro no login", result.Error)
 		return
 	}
@@ -67,7 +125,7 @@ func Login(c *gin.Context) {
 	fmt.Println("input:", input.Password)
 	if valid := auth.VerifyPassword(user.Password, input.Password); !valid {
 		log.Println("Invalid password attempt for user:", user.Email)
-		c.JSON(401, gin.H{"error": "Invalid email or password"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
 		return
 	}
 
@@ -75,11 +133,11 @@ func Login(c *gin.Context) {
 
 	token, err := auth.GenerateToken(user.ID)
 	if err != nil {
-		c.JSON(500, gin.H{"error": "Error generating token"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error generating token"})
 		return
 	}
 	log.Printf("Token generated: %s", token)
-	c.JSON(200, gin.H{"token": token})
+	c.JSON(http.StatusOK, gin.H{"token": token})
 }
 
 func Logout(c *gin.Context) {}
