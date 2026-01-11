ea1075b45c4234c347aa067bff55f80a5d0146ee
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-01-11 20:26:44 -0300

    fix: add cors

--- api/internal/requests/router.go
+++ api/internal/requests/router.go
@@ -3,7 +3,9 @@ package requests
 import (
 	"svitorz/url-shortner/internal/requests/middleware"
 	"svitorz/url-shortner/internal/requests/routes"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +14,15 @@ func SetupRouter() *gin.Engine {
 	r.Use(gin.Logger(), gin.Recovery())
 
 	r.Use(middleware.RateLimiterMiddleware())
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	authMw := middleware.AuthRequired()
 
