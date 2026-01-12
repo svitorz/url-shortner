0b212dedbb2e04a61b5122a9630ea3dba4551701
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-01-11 22:40:19 -0300

    feat: rota inicial

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,13 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PingView from '../views/PingView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
+  linkActiveClass: 'text-gray-900',
+  // link: 'text-gray-500 hover:border-gray-300 hover:text-gray-700',
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: PingView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
     },
   ],
 })
