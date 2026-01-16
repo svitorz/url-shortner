11aaa651af626c16c0bad2256f4428c4498cdadc
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-01-15 23:03:25 -0300

    feat: set routes

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -3,26 +3,31 @@ import PingView from '../views/PingView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
 
+import AccountView from '../views/AccountView.vue'
+import CreateLinkView from '../views/CreateLinkView.vue'
+import LinkDetailsView from '../views/LinkDetailsView.vue'
+import RedirectTesterView from '../views/RedirectTesterView.vue'
+
 const router = createRouter({
   linkActiveClass: 'text-gray-900 border-indigo-600 ',
   history: createWebHashHistory(),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: PingView,
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
-    },
+    { path: '/', name: 'home', component: PingView },
+    { path: '/register', name: 'register', component: RegisterView },
+    { path: '/login', name: 'login', component: LoginView },
+    { path: '/account', name: 'account', component: AccountView, meta: { requiresAuth: true } },
+    { path: '/links/new', name: 'links-new', component: CreateLinkView, meta: { requiresAuth: true } },
+    { path: '/links/:slug', name: 'links-details', component: LinkDetailsView, meta: { requiresAuth: true } },
+    { path: '/test-redirect', name: 'redirect-tester', component: RedirectTesterView },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta?.requiresAuth && !localStorage.getItem('token')) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
